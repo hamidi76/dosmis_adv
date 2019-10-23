716bb6928c8e50bf8269493c69c9b93ec238c055
Hamidi Hamdan <hamidi@stats.gov.my>
2019-10-23 16:55:09 +0800

    fist commit

---
+++ common/config/main-local copy.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=dosmis',
+            'username' => 'root',
+            'password' => 'root@power',
+            'charset' => 'utf8',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'viewPath' => '@common/mail',
+            // send all mails to a file by default. You have to set
+            // 'useFileTransport' to false and configure a transport
+            // for the mailer to send real emails.
+            'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'hamidi.gm@gmail.com',
+                'password' => 'midimi276',
+                'port' => '465',
+                'encryption' => 'SSL',
+                ],
+        ],
+    ],
+];
